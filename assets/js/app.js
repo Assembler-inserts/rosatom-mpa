3fc795b2d6f467ca59eb6c95b38c918c35c7e264
Kussainov Yerlan <drooon41@gmail.com>
2020-09-27 14:40:46 +0700

    Map hotfix

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,9 +12,11 @@ let socket = io('http://45.143.95.87:8888');
 
 let setCoords = (elementId, x, y) => {
     let element = document.getElementById(elementId);
-    element.style.position = 'absolute';
-    element.style.top = y + 'px';
-    element.style.left = x + 'px';
+    if (element !== null) {
+        element.style.position = 'absolute';
+        element.style.top = y + 'px';
+        element.style.left = x + 'px';
+    }
 }
 
 socket.on('connect', function() {
@@ -22,9 +24,8 @@ socket.on('connect', function() {
 });
 
 socket.on('pos', (pos) => {
-    console.log(pos);
-    const x = Math.floor(pos.x / 5 + 300);
-    const y = Math.floor(pos.y / 5 + 300);
+    const x = Math.floor(pos.x / 5 + 250);
+    const y = Math.floor(pos.y / 5 + 250);
     const loaderId = pos.loader;
     const containerId = pos.container;
     setCoords(loaderId, x, y);
